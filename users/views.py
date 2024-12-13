8a342dc3344cff7fa5b2b5f2b4648c3c09aae846
Ismail <ishmael.zouaoui@gmail.com>
2024-12-13 23:52:29 +0100

    Add user authentication views and templates for login and registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("home")  # Change 'home' to your home page URL name
+
+
+class UserRegisterView(CreateView):
+    template_name = "users/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("users:login")
